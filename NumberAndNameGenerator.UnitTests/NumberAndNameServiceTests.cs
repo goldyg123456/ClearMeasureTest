13361be325b0a03a7cbf999747679807db49ac7e
goldyg123456 <gregorygoldsmith@hotmail.com>
2025-04-15 22:17:25 -0500

    Formatting unit tests.

--- NumberAndNameGenerator.UnitTests/NumberAndNameServiceTests.cs
+++ NumberAndNameGenerator.UnitTests/NumberAndNameServiceTests.cs
@@ -9,29 +9,53 @@ namespace NumberAndNameGenerator.UnitTests
         [Fact]
         public void Service_PrintsFullName_WhenDivisibleBy3and5()
         {
-            var result = NumberAndNameService.Generate(15, 3, 5, [new("Greg", "Goldsmith")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 15;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith") };
             var expected = GetExpected("Service_PrintsFullName_WhenDivisibleBy3and5.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 15);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
         public void Service_PrintsFirstName_WhenDivisibleBy3()
         {
-            var result = NumberAndNameService.Generate(3, 3, 5, [new("Greg", "Goldsmith")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 3;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith") };
             var expected = GetExpected("Service_PrintsFirstName_WhenDivisibleBy3.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 3);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
         public void Service_PrintsLastName_WhenDivisibleBy5()
         {
-            var result = NumberAndNameService.Generate(5, 3, 5, [new("Greg", "Goldsmith")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 5;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith") };
             var expected = GetExpected("Service_PrintsLastName_WhenDivisibleBy5.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
             Assert.True(result.Count == 5);
         }
@@ -39,42 +63,72 @@ namespace NumberAndNameGenerator.UnitTests
         [Fact]
         public void Service_PrintsMultipleFullName_WhenDivisibleBy3and5()
         {
-            var result = NumberAndNameService.Generate(15, 3, 5, [new("Greg", "Goldsmith"), new("Tara", "Kohler")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 15;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith"), new("Tara", "Kohler") };
             var expected = GetExpected("Service_PrintsMultipleFullName_WhenDivisibleBy3and5.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 15);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
         public void Service_PrintsMultipleFirstName_WhenDivisibleBy3()
         {
-            var result = NumberAndNameService.Generate(3, 3, 5, [new("Greg", "Goldsmith"), new("Tara", "Kohler")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 3;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith"), new("Tara", "Kohler") };
             var expected = GetExpected("Service_PrintsMultipleFirstName_WhenDivisibleBy3.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 3);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
         public void Service_PrintsMultipleLastName_WhenDivisibleBy5()
-        {
-            var result = NumberAndNameService.Generate(5, 3, 5, [new("Greg", "Goldsmith"), new("Tara", "Kohler")]);
-            var x = JsonConvert.SerializeObject(result);
+        {            // Arrange
+            var iterations = 5;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith"), new("Tara", "Kohler") };
             var expected = GetExpected("Service_PrintsMultipleLastName_WhenDivisibleBy5.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 5);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
         public void Service_PrintsJustNumber_WhenNotDivisibleBy3or5()
         {
-            var nameService = new NumberAndNameService();
-            var result = NumberAndNameService.Generate(2, 3, 5, [new("Greg", "Goldsmith")]);
-            var x = JsonConvert.SerializeObject(result);
+            // Arrange
+            var iterations = 2;
+            var firstNameDivisor = 3;
+            var lastNameDivisor = 5;
+            var names = new List<Name> { new("Greg", "Goldsmith") };
             var expected = GetExpected("Service_PrintsJustNumber_WhenNotDivisibleBy3or5.txt");
+
+            // Act
+            var result = NumberAndNameService.Generate(iterations, firstNameDivisor, lastNameDivisor, names);
+            
+            // Assert
             Assert.Equal(expected, result);
-            Assert.True(result.Count == 2);
+            Assert.True(result.Count == iterations);
         }
 
         [Fact]
